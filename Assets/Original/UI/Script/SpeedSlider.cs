9fa5f3b12f5b73142c42c3404e05188c6147954c
jeumeun <randplore@naver.com>
2025-08-07 16:54:41 +0900

    Slider Sprite 넣기

--- Assets/Original/UI/Script/SpeedSlider.cs
+++ Assets/Original/UI/Script/SpeedSlider.cs
@@ -28,7 +28,7 @@ public class SpeedSlider : MonoBehaviour
 
     void UpdateValueText()
     {
-        speedValue.text =  Mathf.FloorToInt(slider.value) + "km/h";
+        speedValue.text =  Mathf.FloorToInt(sliderUI.GetCurValue()) + "km/h";
 
         float t = slider.value / maxValue;
 
