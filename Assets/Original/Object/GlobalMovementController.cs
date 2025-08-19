5e29b49f04f5a473efb6c5ad4b4c4db08aae00db
chodot <89974193+chodott@users.noreply.github.com>
2025-08-19 10:57:39 +0900

    도로 방향 변경 기능 구현
    - Curvec world 셰이더 적용을 위해 barrier 프리팹 생성
    - 이후 새로운 오브젝트 추가, 차종에 따라 머테리얼 프로퍼티 수정 필요

--- Assets/Original/Object/GlobalMovementController.cs
+++ Assets/Original/Object/GlobalMovementController.cs
@@ -1,13 +1,27 @@
-using System;
+using AmazingAssets.CurvedWorld;
 using UnityEngine;
 
 public class GlobalMovementController : MonoBehaviour
 {
+    [SerializeField]
+    private CurvedWorldController _curvedWorldController;
+    [SerializeField]
+    private float _maxBendSize = 6f;
+
     private PlayerMovement _playerMovement;
+    private float _distanceThreshold = 500f;
+    private float _distanceAccumulator = 0f;
+
+    //(Horizontal, Vertical)
+    private (float, float) _targetBendSize = (3,1);
+    private (float, float) _curBendSize = (3,1);
+
+
     public static GlobalMovementController Instance { get; private set; }
 
     public float GlobalVelocity { get; private set; }
     public float TotalDistance { get; private set; }
+
 
     #region Monobehaviour Callbacks
     private void Awake()
@@ -27,8 +41,21 @@ public class GlobalMovementController : MonoBehaviour
 
     private void FixedUpdate()
     {
-        TotalDistance += GlobalVelocity * Time.fixedDeltaTime;
-        Debug.Log($"totalDistance: {TotalDistance}");
+        float distance = GlobalVelocity * Time.fixedDeltaTime;
+        TotalDistance += distance;
+        _distanceAccumulator += distance;
+
+        float lerpAlpha = _distanceAccumulator / _distanceThreshold;
+        _curBendSize.Item1 = Mathf.Lerp(_curBendSize.Item1, _targetBendSize.Item1, lerpAlpha);
+        _curBendSize.Item2 = Mathf.Lerp(_curBendSize.Item2, _targetBendSize.Item2, lerpAlpha);
+        _curvedWorldController.bendHorizontalSize = _curBendSize.Item1;
+        _curvedWorldController.bendVerticalSize = _curBendSize.Item2;
+
+        if (_distanceAccumulator >= _distanceThreshold)
+        {
+            SetRoadRandomBend();
+            _distanceAccumulator = 0f;
+        }
     }
     #endregion
 
@@ -38,10 +65,14 @@ public class GlobalMovementController : MonoBehaviour
         _playerMovement.OnSpeedChanged += HandleVelocityChanged;
     }
 
-
-
     private void HandleVelocityChanged(float velocity)
     {
         GlobalVelocity = velocity;
+    }
+
+    private void SetRoadRandomBend()
+    {
+        _targetBendSize.Item1 = Random.Range(-1f, 1f) * _maxBendSize;
+        _targetBendSize.Item2 = Random.Range(-1f, 1f) + _maxBendSize;
     }
 }
