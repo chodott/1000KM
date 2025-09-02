9312e6cec0d9deed513ef9d7d7bdcf0bfeff607b
chodot <89974193+chodott@users.noreply.github.com>
2025-09-02 17:59:20 +0900

    모션 블러 적용

--- Assets/Original/Object/Effect/Script/FakeMotionFeeder.cs
+++ Assets/Original/Object/Effect/Script/FakeMotionFeeder.cs
@@ -5,14 +5,14 @@ public class FakeMotionFeeder : MonoBehaviour
 {
     public Vector3 worldDirection = -Vector3.right;
 
-    [Tooltip("0이면 매 프레임 즉시 반영. 값을 올리면 델타가 서서히 따라감")]
     public float smoothing = 0f;
+    public float _blurScale = 0.1f;
 
     static readonly int ShaderId_FakeLocalDelta = Shader.PropertyToID("_FakeLocalDelta");
 
     readonly List<Renderer> _renderers = new List<Renderer>();
     MaterialPropertyBlock _mpb;
-    Vector3 _currentLocalDelta;  // 스무딩용 누적값
+    Vector3 _currentLocalDelta; 
 
     void Awake()
     {
@@ -23,17 +23,15 @@ public class FakeMotionFeeder : MonoBehaviour
     void LateUpdate()
     {
         float curVelocity = GlobalMovementController.Instance.GlobalVelocity;
-        Vector3 fakeWorldDelta = worldDirection * curVelocity * Time.deltaTime;
+        Vector3 fakeWorldDelta = worldDirection * curVelocity * Time.deltaTime * _blurScale;
 
         Vector3 targetLocalDelta = transform.InverseTransformDirection(fakeWorldDelta);
 
-        // 3) 스무딩 옵션 적용
         if (smoothing > 0f)
             _currentLocalDelta = Vector3.Lerp(_currentLocalDelta, targetLocalDelta, 1f - Mathf.Exp(-smoothing * Time.deltaTime));
         else
             _currentLocalDelta = targetLocalDelta;
 
-        // 4) 모든 렌더러에 MPB로 주입
         foreach (var r in _renderers)
         {
             if (!r) continue;
